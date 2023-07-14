dace8903b8998a7ff5912aadd03f7d534f0c064c
Sujal Singh <email.sujalsingh@gmail.com>
2023-07-14 12:21:17 +0530

    build workflow

---
+++ build.sh
@@ -0,0 +1,19 @@
+# Build PDF
+mkdir -p ./out
+pdflatex -interaction=nonstopmode -output-format=pdf -output-directory=./out ./src/resume.tex
+cd ./out || exit
+
+# Clean up build files
+find ./* ! -name "resume.pdf" -delete
+
+# Build Preview Images
+pdftoppm resume.pdf preview -png
+
+# Clear GitHub image cache
+IMAGES=$(
+  curl -s "https://github.com/sujaldev/resume/blob/main/README.md" | grep --color=never -Poi "https:\/\/camo[^\"]*"
+)
+
+for IMAGE in $IMAGES; do
+  curl -X PURGE "$IMAGE"
+done
